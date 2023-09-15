d0270128e3e4ed1c064fe13b52a912cdc8dc17e2
mohan13 <mohangrg250@gmail.com>
2023-09-15 19:20:37 +0545

    added hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '@/HOC/layout';
+import Hotjar from '@hotjar/browser';
 import Head from 'next/head';
 export default function App({ Component, pageProps }: AppProps) {
+  const siteId = 3655935;
+  const hotjarVersion = 6;
+  Hotjar.init(siteId, hotjarVersion);
   return (
     <>
       <Head>
