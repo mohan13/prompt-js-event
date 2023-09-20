d80edb7017ec9e28607c12545cb8cc38fc3d71ee
mohan13 <mohangrg250@gmail.com>
2023-09-20 10:47:37 +0545

    toggle menu added

--- src/components/Navigation/navitems.tsx
+++ src/components/Navigation/navitems.tsx
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Router, { useRouter } from 'next/router';
+import { FiMenu } from 'react-icons/fi';
 import { menu } from '../Constant/menu';
 const Navitems = () => {
   const router = useRouter();
+  const [open, setOpen] = useState(false);
   return (
-    <div className=" w-64 h-screen ">
-      <div className="overflow-y-auto py-4 px-3 h-full bg-white border-r border-gray-200 ">
-        <div className="space-y-2">
-          {menu?.map((item) => {
-            return (
-              <div key={item.id}>
-                <Link
-                  href={item.url}
-                  className={`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100 group ${
-                    router.pathname === item.url ? 'bg-gray-100' : ''
-                  }`}
-                >
-                  <div className="ml-3">{item.title}</div>
-                </Link>
-              </div>
-            );
-          })}
+    <>
+      <button
+        className="border-2 border-black m-2 p-2"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        <FiMenu className="h-10 w-10" />
+      </button>
+      {open && (
+        <div className="w-64 h-screen z-10 absolute">
+          <div className="overflow-y-auto flex py-4 px-3 h-full bg-white border-r border-gray-200 ">
+            <div className="space-y-2">
+              {menu?.map((item) => {
+                return (
+                  <div key={item.id}>
+                    <Link
+                      onClick={() => setOpen(false)}
+                      href={item.url}
+                      className={`flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100 group ${
+                        router.pathname === item.url ? 'bg-gray-100' : ''
+                      }`}
+                    >
+                      <div className="ml-3">{item.title}</div>
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
