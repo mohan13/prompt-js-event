0ea651b5ddeeda63c737b18c6fdfb5b3cc599ab9
mohan13 <mohangrg250@gmail.com>
2023-09-24 12:36:04 +0545

    height fixed

--- src/components/ReusableComponents/banner_1.tsx
+++ src/components/ReusableComponents/banner_1.tsx
@@ -26,7 +26,7 @@ const Banner = ({
   };
   return (
     <motion.div className=" body-font">
-      <div className="w-full bg-[#faeee7] mx-auto flex px-5 py-5 md:pt-24 md:flex-row flex-col items-center">
+      <div className="w-full h-screen bg-[#faeee7] mx-auto flex px-5 py-5 md:pt-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 pr-0 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center">
           <motion.div
             initial={{ x: -50, opacity: 0 }}
