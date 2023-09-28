335a01ebf45aadb30162805caf800ed3ad07424d
mohan13 <mohangrg250@gmail.com>
2023-09-28 09:45:37 +0545

    thank you

--- src/pages/summery.tsx
+++ src/pages/summery.tsx
@@ -3,10 +3,15 @@ import React from 'react';
 const index = () => {
   return (
     <div className="relative inset-x-2/3 top-32 grid px-5 pt-5 md:pt-24 text-center items-center ">
-      <h1 className="title-font sm:text-6xl text-3xl mb-4 font-medium text-gray-900">
-        End of Slides
+      <h1 className="title-font sm:text-6xl text-3xl mb-4 font-medium text-[#33272a]">
+        Thank You !
       </h1>
-      <div className="text-[#ff8ba7] sm:text-4xl text-2xl">Thank You.</div>
+      <div className="text-[#594a4e] sm:text-4xl text-2xl">
+        Thank you for joining our PromptJS event on "Collect data to Enhance
+        User Experience". We hope this session has provided you with valuable
+        insights and inspiration to take your frontend development career to new
+        heights. Best of luck on your journey!
+      </div>
     </div>
   );
 };
