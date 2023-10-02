cd266cdb76b25ae147b4dc1c824e6ae3a3f32aed
mohan13 <mohangrg250@gmail.com>
2023-10-02 17:47:08 +0545

    testing redirection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,17 @@ import Layout from '@/HOC/layout';
 import Hotjar from '@hotjar/browser';
 import { hotjar_site_id, hotjar_version } from '@/config';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   useEffect(() => {
-    Hotjar.init(hotjar_site_id, hotjar_version);
-  }, []);
+    if (router.query.from === 'skill') {
+      console.log('redirection', router.push('/'));
+      router.push('/');
+    }
+
+    Hotjar.init(hotjar_version, hotjar_site_id);
+  }, [router.pathname, router.query.from]);
 
   return (
     <>
